39b570aa6e0bfd500fdeadb684509f128a5c769a
Baranskij <82298610+Baranskij@users.noreply.github.com>
2021-07-15 02:02:47 +0700

    add

---
+++ constants/catalog.js
@@ -0,0 +1,28 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Камень',
+        img: 'img/stone/green/007.jpg',
+        price: 0,
+    },
+    {
+        id: 'el2',
+        name: 'Камень2',
+        img: 'img/stone/yellow/154.jpg',
+        price: 0,
+    },
+    {
+        id: 'el3',
+        name: 'Камень3',
+        img: 'img/stone/red/9579.jpg',
+        price: 0,
+    },
+    {
+        id: 'el4',
+        name: 'Камень4',
+        img: 'img/stone/other/0013.jpg',
+        price: 0,
+    }
+
+
+]
